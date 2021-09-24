e945d87ee39924d25efc5160c701809ca2e0f63d
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-09-24 19:37:53 +0530

    Enqueue method performed to adding element in queue

--- StackQueue/Program.cs
+++ StackQueue/Program.cs
@@ -7,7 +7,7 @@ namespace StackQueue
         static void Main(string[] args)
         {
             Console.WriteLine("WELCOME TO STACK AND QUEUE PROGRAM");
-            StackLinkedList<int> stack = new StackLinkedList<int>();//creating object 
+            /*StackLinkedList<int> stack = new StackLinkedList<int>();//creating object 
             stack.push(70);
             stack.push(30);
             stack.push(56);
@@ -18,7 +18,14 @@ namespace StackQueue
             stack.pop();
             Console.WriteLine("*******************************");
             stack.isEmpty();
-            stack.display();
+            stack.display();*/
+
+            QueueLinkedList<int> queue = new QueueLinkedList<int>();
+            queue.Enqueue(70);
+            queue.Enqueue(30);
+            queue.Enqueue(56);
+            queue.Display();
+
 
 
         }
---
+++ StackQueue/QueueLinkedList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackQueue
+{
+    class QueueLinkedList<Gtype>
+    {
+        NodeStack<Gtype> head = null;
+
+        public void Enqueue(Gtype data)
+        {
+            NodeStack<Gtype> node = new NodeStack<Gtype>(data);
+            if(this.head==null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                NodeStack<Gtype> temp = head;
+                while(temp.next!=null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} Element added in Queue" + node.data);
+        }
+
+        public void Display()
+        {
+            NodeStack<Gtype> temp = this.head;
+            if(temp==null)
+            {
+                Console.WriteLine("Queue is Empty");
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }         
+        }
+    }
+}
